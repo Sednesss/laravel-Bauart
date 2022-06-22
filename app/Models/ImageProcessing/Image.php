171d4d38120fc3bdc450a2e39b64c39d86c8730a
Sednesss <mosquito.01@mail.ru>
2022-06-22 18:15:36 +0700

    add model and migrations

--- app/Models/ImageProcessing/Image.php
+++ app/Models/ImageProcessing/Image.php
@@ -2,10 +2,21 @@
 
 namespace App\Models\ImageProcessing;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
     use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function storage()
+    {
+        return $this->belongsTo(Storage::class);
+    }
 }
--- database/migrations/2022_06_22_083643_create_images_table.php
+++ database/migrations/2022_06_22_083643_create_images_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -16,8 +17,8 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->bigIncrements('id');
 
-            $table->integer('user_id');
-            $table->integer('storage_id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('storage_id')->unsigned();
 
             $table->string('name');
             $table->string('path_origin');
